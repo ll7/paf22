7b2fad43aae0821d8d17ad27c3e5e314ec9a0362
Korbinian Stein <korbinian.stein@uni-a.de>
2022-11-22 09:24:23 +0100

    feat(#27): Make car drive straight with 1m/s

--- code/acting/src/acting/acting.py
+++ code/acting/src/acting/acting.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import ros_compatibility as roscomp
 from ros_compatibility.node import CompatibleNode
+from ackermann_msgs.msg import AckermannDrive
+from rospy import Publisher
 
 
 class Acting(CompatibleNode):
@@ -8,13 +10,25 @@ class Acting(CompatibleNode):
         super(Acting, self).__init__('acting')
         self.loginfo('Acting node started')
 
-        self.control_loop_rate = self.get_param('control_loop_rate', 10)
+        self.control_loop_rate = self.get_param('control_loop_rate', 0.1)
+        self.role_name = self.get_param('role_name', 'ego_vehicle')
+
+        self.ackermann_publisher: Publisher = self.new_publisher(
+            AckermannDrive,
+            f'/carla/{self.role_name}/ackermann_cmd',
+            qos_profile=0
+        )
 
     def run(self):
         self.loginfo('Acting node running')
 
         def loop(timer_event=None):
             self.loginfo('Acting node loop')
+            message = AckermannDrive()
+            message.speed = 1
+            self.ackermann_publisher.publish(message)
+            self.loginfo('Acting message published')
+            print("test")
 
         self.new_timer(self.control_loop_rate, loop)
         self.spin()

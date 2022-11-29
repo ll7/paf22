1718f843f885254af35958301f8ab1995b4d4c89
Korbinian Stein <korbinian.stein@uni-a.de>
2022-11-29 09:52:46 +0100

    fix(#27): Timestamp errors with control loop rate

--- code/acting/src/acting/acting.py
+++ code/acting/src/acting/acting.py
@@ -4,7 +4,7 @@ from ros_compatibility.node import CompatibleNode
 from carla_msgs.msg import CarlaEgoVehicleControl
 from rospy import Publisher
 from ros_compatibility.qos import QoSProfile, DurabilityPolicy
-from std_msgs.msg import Bool, Header
+from std_msgs.msg import Bool
 
 
 class Acting(CompatibleNode):
@@ -12,13 +12,13 @@ class Acting(CompatibleNode):
         super(Acting, self).__init__('acting')
         self.loginfo('Acting node started')
 
-        self.control_loop_rate = self.get_param('control_loop_rate', 0.1)
+        self.control_loop_rate = self.get_param('control_loop_rate', 0.05)
         self.role_name = self.get_param('role_name', 'ego_vehicle')
 
         self.control_publisher: Publisher = self.new_publisher(
             CarlaEgoVehicleControl,
             f'/carla/{self.role_name}/vehicle_control_cmd',
-            qos_profile=1
+            qos_profile=10
         )
         self.status_pub = self.new_publisher(
             Bool, f"/carla/{self.role_name}/status",
@@ -26,28 +26,28 @@ class Acting(CompatibleNode):
                 depth=1,
                 durability=DurabilityPolicy.TRANSIENT_LOCAL)
         )
-        self.status_pub.publish(True)
 
     def run(self):
+        self.status_pub.publish(True)
         self.loginfo('Acting node running')
 
         def loop(timer_event=None):
-            self.loginfo('Acting node loop')
             message = CarlaEgoVehicleControl()
-
-            # create header with correct timestamp
-            header = Header()
-            header.frame_id = 'map'
-            header.stamp = roscomp.ros_timestamp(sec=self.get_time(),
-                                                 from_sec=True)
-            message.header = header
 
             # set throttle to 0.1
             message.throttle = 0.1
+            message.brake = 0
+            message.hand_brake = False
+            message.manual_gear_shift = False
+
+            message.header.stamp = roscomp.ros_timestamp(self.get_time(),
+                                                         from_sec=True)
             self.control_publisher.publish(message)
-            self.loginfo('Acting message published')
-            self.loginfo(message)
-            self.loginfo(self.get_time())
+
+            # debug
+            self.loginfo('header: {}'.format(message.header))
+            self.loginfo(
+                roscomp.ros_timestamp(self.get_time(), from_sec=True))
 
         self.new_timer(self.control_loop_rate, loop)
         self.spin()

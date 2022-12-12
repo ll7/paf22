6b57ed233e33480e6f2069fd1909dfc34ae1b30f
Julian-Graf <julian.graf@student.uni-augsburg.de>
2022-12-12 13:13:40 +0100

    feat(#72): Add comments to code

--- code/acting/src/acting/vehicle_controller.py
+++ code/acting/src/acting/vehicle_controller.py
@@ -6,12 +6,20 @@ from rospy import Publisher, Subscriber
 from ros_compatibility.qos import QoSProfile, DurabilityPolicy
 from std_msgs.msg import Bool, Float32
 
-PURE_PURSUIT_CONTROLLER = 1
-STANLEY_CONTROLLER = 2
+PURE_PURSUIT_CONTROLLER: int = 1
+STANLEY_CONTROLLER: int = 2
 
 
-# todo: docs
 class VehicleController(CompatibleNode):
+    """
+    This node is responsible for collecting all data needed for the
+    vehicle_control_cmd and sending it.
+    The node also decides weather to use the stanley or pure pursuit
+    controller.
+    If the node receives an emergency msg, it will bring the vehicle to a stop
+    and send an emergency msg with data = False back, after the velocity of the
+    vehicle has reached 0.
+    """
     def __init__(self):
         super(VehicleController, self).__init__('vehicle_controller')
         self.loginfo('VehicleController node started')
@@ -24,14 +32,6 @@ class VehicleController(CompatibleNode):
             f'/carla/{self.role_name}/vehicle_control_cmd',
             qos_profile=10
         )
-        self.status_pub: Publisher = self.new_publisher(
-            Bool,
-            f"/carla/{self.role_name}/status",
-            qos_profile=QoSProfile(
-                depth=1,
-                durability=DurabilityPolicy.TRANSIENT_LOCAL)
-        )
-
         self.status_pub: Publisher = self.new_publisher(
             Bool,
             f"/carla/{self.role_name}/status",
@@ -83,15 +83,15 @@ class VehicleController(CompatibleNode):
 
     def run(self):
         """
-        Starts the main loop of the node
+        Starts the main loop of the node and send a status msg.
         :return:
         """
         self.status_pub.publish(True)
         self.loginfo('VehicleController node running')
 
-        def loop(timer_event=None):
+        def loop(timer_event=None) -> None:
             """
-            Main loop of the acting node
+            Collects all data received and sends a CarlaEgoVehicleControl msg.
             :param timer_event: Timer event from ROS
             :return:
             """
@@ -122,6 +122,7 @@ class VehicleController(CompatibleNode):
             message.manual_gear_shift = False
             message.steer = steer
             # message.throttle = 5  # for testing todo: delete
+            message.gear = 1
             message.header.stamp = roscomp.ros_timestamp(self.get_time(),
                                                          from_sec=True)
             self.control_publisher.publish(message)
@@ -129,7 +130,12 @@ class VehicleController(CompatibleNode):
         self.new_timer(self.control_loop_rate, loop)
         self.spin()
 
-    def __emergency_break(self, data):
+    def __emergency_break(self, data) -> None:
+        """
+        Executes emergency stop
+        :param data:
+        :return:
+        """
         if not data.data:  # not an emergency
             return
         if self.__emergency:  # emergency was already triggered
@@ -138,7 +144,7 @@ class VehicleController(CompatibleNode):
         self.__emergency = True
         message = CarlaEgoVehicleControl()
         message.throttle = 1
-        message.steer = 1  # todo: look up 30 degree angle
+        message.steer = 1  # todo: maybe use 30 degree angle
         message.brake = 1
         message.reverse = True
         message.hand_brake = True
@@ -147,7 +153,13 @@ class VehicleController(CompatibleNode):
                                                      from_sec=True)
         self.control_publisher.publish(message)
 
-    def __get_velocity(self, data: CarlaSpeedometer):
+    def __get_velocity(self, data: CarlaSpeedometer) -> None:
+        """
+        Ends emergency if vehicle velocity reaches 0. Sends emergency msg
+        with data = False after stopping.
+        :param data:
+        :return:
+        """
         if not self.__emergency:  # nothing to do in this case
             return
         if data.speed < 0.1:  # vehicle has come to a stop
@@ -177,8 +189,12 @@ class VehicleController(CompatibleNode):
     def __set_stanley_steer(self, data):
         self.__stanley_steer = data.data
 
-    def __choose_controller(self):  # todo: implement
-        return PURE_PURSUIT_CONTROLLER
+    def __choose_controller(self) -> int:  # todo: implement
+        """
+        Chooses with steering controller to use
+        :return:
+        """
+        return STANLEY_CONTROLLER
 
 
 def main(args=None):

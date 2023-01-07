2ac34f2a4c7950c2d5cfe3e35b949aef5469e0f0
schwalga <schwagabriel+git@gmail.com>
2023-01-07 15:51:00 +0100

    fix(#67): Forgot to add files to the commit :|

--- code/acting/src/acting/pure_pursuit_controller.py
+++ code/acting/src/acting/pure_pursuit_controller.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
+import math
 from math import atan, sin
 
-import numpy
 import ros_compatibility as roscomp
 from carla_msgs.msg import CarlaSpeedometer
-from geometry_msgs.msg import Point, Pose
+from geometry_msgs.msg import Point, Pose, PoseStamped
 from nav_msgs.msg import Path
 from ros_compatibility.node import CompatibleNode
 from ros_compatibility.qos import QoSProfile, DurabilityPolicy
 from rospy import Publisher, Subscriber
-from sensor_msgs.msg import NavSatFix, Imu
+from sensor_msgs.msg import Imu
 from std_msgs.msg import Float32, Bool
-from tf.transformations import euler_from_quaternion
+
+from helper_functions import vectors2angle, get_vector_direction
 
 
 # todo: docs
@@ -26,7 +27,7 @@ class PurePursuitController(CompatibleNode):
 
         self.position_sub: Subscriber = self.new_subscription(
             Path,
-            f"/carla/{self.role_name}/path",
+            f"/carla/{self.role_name}/trajectory",
             self.__set_path,
             qos_profile=1)
 
@@ -37,8 +38,8 @@ class PurePursuitController(CompatibleNode):
             qos_profile=1)
 
         self.path_sub: Subscriber = self.new_subscription(
-            NavSatFix,
-            f"/carla/{self.role_name}/GPS",
+            PoseStamped,
+            f"/carla/{self.role_name}/current_pos",
             self.__set_position,
             qos_profile=1)
 
@@ -61,9 +62,10 @@ class PurePursuitController(CompatibleNode):
                 durability=DurabilityPolicy.TRANSIENT_LOCAL)
         )
 
-        self.__position: (float, float) = None  # latitude, longitude in deg
+        self.__position: (float, float) = None  # x, y
+        self.__last_pos: (float, float) = None
         self.__path: Path = None
-        self.__heading: float = None
+        self.__heading: float = None  # currently unusable as x,y are to big
         self.__velocity: float = None
 
     def run(self):
@@ -72,9 +74,8 @@ class PurePursuitController(CompatibleNode):
         :return:
         """
         self.loginfo('PurePursuitController node running')
-        self.status_pub.publish(True)  # todo: delete (this is only
-
-        # necessary if vehicle controller isn't running)
+        self.status_pub.publish(True)  # todo: delete this is only
+        # necessary if vehicle controller isn't running
 
         def loop(timer_event=None):
             """
@@ -108,48 +109,191 @@ class PurePursuitController(CompatibleNode):
         self.new_timer(self.control_loop_rate, loop)
         self.spin()
 
-    def __set_position(self, data: NavSatFix):
-        self.__position = (data.latitude, data.longitude)
+    def __set_position(self, data: PoseStamped):
+        if self.__position is not None:
+            old_x = self.__position[0]
+            old_y = self.__position[1]
+            self.__last_pos = (old_x, old_y)
+
+        new_x = data.pose.position.x
+        new_y = data.pose.position.y
+        self.__position = (new_x, new_y)
 
     def __set_path(self, data: Path):
         self.__path = data
 
     def __set_heading(self, data: Imu):  # todo: test
-        rot_euler = euler_from_quaternion(data.orientation)
-        self.__heading = rot_euler[2]
+        if self.__position is None:
+            self.logerr("__get_heading: Current Position not set")
+            self.__heading = 0
+            return
+        if self.__last_pos is None:
+            self.logerr("__get_heading: Last Position not set")
+            self.__heading = 0
+            return
+        cur_x = self.__position[0] - self.__last_pos[0]
+        cur_y = self.__position[1] - self.__last_pos[1]
+        self.__heading = vectors2angle(cur_x, cur_y, 1, 0)
 
     def __set_velocity(self, data):
         self.__velocity = data.speed
 
     def __calculate_steer(self) -> float:
-        L = 2  # dist between front and rear wheels todo: measure
-        K = 10  # todo: tune
-        alpha = self.__get_heading_error()
-        look_ahead_dist = K * self.__velocity
-        steering_angle = atan((2 * L * sin(alpha)) / look_ahead_dist)
+        l_vehicle = 2  # dist between front and rear wheels todo: measure
+        k = 10  # todo: tune
+
+        current_velocity: float
+        if self.__velocity == 0:
+            current_velocity = k * 0.1
+        else:
+            current_velocity = self.__velocity
+
+        look_ahead_dist = k * current_velocity
+        target_wp_idx = self.__get_target_point_index(look_ahead_dist)
+
+        target_wp: PoseStamped = self.__path.poses[target_wp_idx]
+        target_v_x = target_wp.pose.position.x - self.__last_pos[0]
+        target_v_y = target_wp.pose.position.y - self.__last_pos[1]
+        cur_v_x = self.__position[0] - self.__last_pos[0]
+        cur_v_y = self.__position[1] - self.__last_pos[1]
+
+        alpha = vectors2angle(target_v_x, target_v_y,
+                              cur_v_x, cur_v_y)
+        steering_angle = atan((2 * l_vehicle * sin(alpha)) / look_ahead_dist)
+
+        # for debugging only ->
+        # t_x = target_wp.pose.position.x
+        # t_y = target_wp.pose.position.y
+        # c_x = self.__position[0]
+        # c_y = self.__position[1]
+        self.loginfo(
+                    f"T_V: ({round(target_v_x, 3)},{round(target_v_y, 3)}) \t"
+                    # f"T_WP: ({round(t_x,3)},{round(t_y,3)}) \t"
+                    # f"C_WP: ({round(c_x,3)},{round(c_y,3)}) \t"
+                    f"Target Steering angle: {round(steering_angle, 4)} \t"
+                    # f"Current alpha: {round(alpha, 6)} \t"
+                    # f"Target WP idx: {target_wp_idx}"
+                    f"Current heading: {round(self.__heading, 4)}"
+                    )
+        # <- for debugging only
+
         return steering_angle
+
+    def __get_target_point_index(self, ld: float) -> int:
+        """
+        Get the index of the target point on the current trajectory based on
+        the look ahead distance.
+        :param ld: look ahead distance
+        :return:
+        """
+        if len(self.__path.poses) < 2:
+            return -1
+
+        min_dist = 10e1000
+        min_dist_idx = -1
+        for i in range(0, len(self.__path.poses)):
+            pose: PoseStamped = self.__path.poses[i]
+            dist = self.__dist_to(pose.pose.position)
+            dist2ld = dist - ld
+            if min_dist > dist2ld > 0:
+                min_dist = dist2ld
+                min_dist_idx = i
+        return min_dist_idx
 
     def __get_heading_error(self) -> float:
         my_heading = self.__heading
-        target_rot = self.__get_closest_point_on_path().orientation
-        rot_euler = euler_from_quaternion(target_rot)
-        target_heading = rot_euler[2]
+        target_idx = self.__get_closest_point_on_path_index()
+
+        # for debugging only ->
+        target_pose: PoseStamped = self.__path.poses[target_idx]
+        tp_x, tp_y = target_pose.pose.position.x, target_pose.pose.position.y
+        self.loginfo(f"Target position: \t x: {tp_x} \t y: {tp_y} |"
+                     f"| Current position: x: {self.__position[0]} "
+                     f"\t y: {self.__position[1]}")
+        # <- for debugging only
+
+        target_heading = self.__get_pose_heading(target_idx)
+        self.loginfo(f"target heading: {target_heading} "
+                     f"\t current_heading: {my_heading}")
         return target_heading - my_heading
+
+    def __get_pose_heading(self, pose_idx: int) -> float:
+        """
+        Given the index of a pose this method returns the angle
+        relative to the x-axis.
+        :param pose_idx: Index of the pose
+        :return: average angle of the pose
+        """
+        target_idx = pose_idx
+        target_point: PoseStamped
+        target_point = self.__path.poses[target_idx]
+
+        # get the previous and next point on the path
+        path_len = len(self.__path.poses)
+        tp_after: PoseStamped
+        tp_after = self.__path.poses[min(path_len, target_idx + 1)]
+        tp_prev: PoseStamped
+        tp_prev = self.__path.poses[max(0, target_idx - 1)]
+
+        # calculate the average direction of the two vectors
+        # v1 = tp_before -> tp
+        # v2 = tp -> tp_after
+        avg_heading = 0.0
+        avg_heading_args = 0
+        tp_x = target_point.pose.position.x
+        tp_y = target_point.pose.position.y
+        if tp_prev is not target_point:
+            tp_prev_x = tp_prev.pose.position.x
+            tp_prev_y = tp_prev.pose.position.y
+            avg_heading += get_vector_direction(tp_prev_x, tp_prev_y,
+                                                tp_x, tp_y)
+            avg_heading_args += 1
+        if tp_after is not target_point:
+            tp_after_x = tp_after.pose.position.x
+            tp_after_y = tp_after.pose.position.y
+            avg_heading += get_vector_direction(tp_x, tp_y,
+                                                tp_after_x, tp_after_y)
+            avg_heading_args += 1
+        if avg_heading_args == 0:
+            raise Exception()
+        return avg_heading / avg_heading_args
+
+    def __get_closest_point_on_path_index(self) -> int:
+        min_dist: float = 10e1000
+        min_pos_index = -1
+        for i in range(0, len(self.__path.poses)):
+            pose: PoseStamped
+            pose = self.__path.poses[i]
+            if self.__dist_to(pose.pose.position) <= min_dist:
+                min_dist = self.__dist_to(pose.pose.position)
+                min_pos_index = i
+        if min_pos_index == -1:
+            raise Exception()
+        return min_pos_index
 
     def __get_closest_point_on_path(self) -> Pose:
         min_dist: float = 10e1000
         min_pos: Pose = None
         for pose in self.__path.poses:
-            if self.__dist_to(pose.position) <= min_dist:
-                min_dist = self.__dist_to(pose.position)
-                min_pos = pose
+            if self.__dist_to(pose.pose.position) <= min_dist:
+                min_dist = self.__dist_to(pose.pose.position)
+                min_pos = pose.pose
         if min_pos is None:
             raise Exception()
         return min_pos
 
     def __dist_to(self, pos: Point) -> float:
-        my_pos = numpy.array((self.__position[0], self.__position[1], pos[2]))
-        return numpy.linalg.norm(my_pos - pos)
+        """
+        Distance between current position and target position (only (x,y))
+        :param pos: targeted position
+        :return: distance
+        """
+        x_current = self.__position[0]
+        y_current = self.__position[1]
+        x_target = pos.x
+        y_target = pos.y
+        d = (x_target - x_current)**2 + (y_target - y_current)**2
+        return math.sqrt(d)
 
 
 def main(args=None):

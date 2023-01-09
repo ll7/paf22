24285b32a3b8cd2fd52059df55fd3193570ccea7
Julian-Graf <julian.graf@student.uni-augsburg.de>
2023-01-09 17:50:03 +0100

    feat(#108): Add rotation

--- code/acting/src/acting/MainFramePublisher.py
+++ code/acting/src/acting/MainFramePublisher.py
@@ -4,11 +4,18 @@ import rospy
 from geometry_msgs.msg import Pose
 from ros_compatibility.node import CompatibleNode
 import tf
+from scipy.spatial.transform import Rotation as R
 
 
 class MainFramePublisher(CompatibleNode):
 
     def __init__(self):
+        """
+        This node handles the translation from the static main frame to the
+        moving hero frame. The hero frame always moves and rotates as the
+        ego vehicle does. The hero frame is used by sensors like the lidar.
+        Rviz also uses the hero frame. The main frame is used for planning.
+        """
         super(MainFramePublisher, self).__init__('main_frame_publisher')
         self.loginfo('MainFramePublisher node started')
 
@@ -27,11 +34,16 @@ class MainFramePublisher(CompatibleNode):
         br = tf.TransformBroadcaster()
 
         def loop(timer_event=None):
+            # Invert Translation and Rotation to transform from main
+            # to hero frame
             pos = (-self.current_pos.position.x,
                    -self.current_pos.position.y,
                    -self.current_pos.position.z)
+            curr_rot = self.current_pos.orientation
+            rot_quat = [curr_rot.x, curr_rot.y, curr_rot.z, curr_rot.w]
+            invert_rot_quat = R.from_quat(rot_quat).inv().as_quat()
             br.sendTransform(pos,
-                             (0.0, 0.0, 0.0, 1.0),
+                             invert_rot_quat,
                              rospy.Time.now(),
                              "main",
                              "hero",

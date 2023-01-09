4c2d90c80e0988f26c0383b4e07c5d6e663c694e
JoKircher <josef_kircher@gmx.de>
2023-01-09 10:14:24 +0100

    feat(#119): modify example

--- code/planning/eX/TEST.py
+++ code/planning/eX/TEST.py
@@ -2,11 +2,12 @@
 
 import rospy
 import smach
-
+import smach_ros
 
 # define state Foo
-class Foo(smach.State):
 
+
+class Foo(smach.State):
     def __init__(self):
         smach.State.__init__(self, outcomes=['outcome1', 'outcome2'])
         self.counter = 0
@@ -29,9 +30,8 @@ class Bar(smach.State):
         rospy.loginfo('Executing state BAR')
         return 'outcome2'
 
+
 # main
-
-
 def main():
     rospy.init_node('smach_example_state_machine')
 
@@ -47,8 +47,17 @@ def main():
         smach.StateMachine.add('BAR', Bar(),
                                transitions={'outcome2': 'FOO'})
 
+    # Create and start the introspection server
+    sis = smach_ros.IntrospectionServer('my_smach_introspection_server',
+                                        sm, '/SM_ROOT')
+    sis.start()
+
     # Execute SMACH plan
     sm.execute()
+
+    # Wait for ctrl-c to stop the application
+    rospy.spin()
+    sis.stop()
 
 
 if __name__ == '__main__':

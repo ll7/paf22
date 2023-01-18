5579cfeca4f8d96b404a6c5af68a716e4bb0ef31
schwalga <schwagabriel+git@gmail.com>
2023-01-18 16:38:15 +0100

    fix(#131): Got EKF to work. Values for covariance need tweaking

--- code/perception/src/EKFTranslationNode.py
+++ code/perception/src/EKFTranslationNode.py
@@ -6,11 +6,13 @@ It therefore receives a nav_msgs/Path msg.
 """
 
 import ros_compatibility as roscomp
+# import rospy
 from ros_compatibility.node import CompatibleNode
 # from carla_msgs.msg import CarlaSpeedometer
-from sensor_msgs.msg import NavSatFix
+from sensor_msgs.msg import NavSatFix, Imu
 # from geometry_msgs.msg import PoseStamped
 from nav_msgs.msg import Odometry
+from coordinate_transformation import CoordinateTransformer, GeoRef
 
 # import rospy
 
@@ -33,8 +35,21 @@ class EKFTranslation(CompatibleNode):
         self.role_name = self.get_param("role_name", "hero")
         self.control_loop_rate = self.get_param("control_loop_rate", "0.05")
 
+        self.transformer = CoordinateTransformer()
+        gps_ref = GeoRef.TOWN12
+        lat0 = gps_ref.value[0]
+        lon0 = gps_ref.value[1]
+        h0 = gps_ref.value[2]
+        self.transformer.set_gnss_ref(lat0, lon0, h0)
+
         # Subscriber
-        self.gnss_subscriber = self.new_subscription(
+        self.imu_subscriber = self.new_subscription(
+            Imu,
+            "/carla/" + self.role_name + "/IMU",
+            self.update_imu_data,
+            qos_profile=1)
+
+        self.gps_subscriber = self.new_subscription(
             NavSatFix,
             "/carla/" + self.role_name + "/GPS",
             self.update_gps_data,
@@ -44,14 +59,79 @@ class EKFTranslation(CompatibleNode):
         # 2D Odometry (Maybe Speedometer?)
         self.ekf_odom_publisher = self.new_publisher(
             Odometry,
-            "/carla/" + self.role_name + "/ekf_odom",
+            "/odom",
+            qos_profile=1)
+
+        # IMU
+        self.ekf_imu_publisher = self.new_publisher(
+            Imu,
+            "/imu_data",
             qos_profile=1)
 
         # 3D Odomoetry (GPS)
         self.ekf_vo_publisher = self.new_publisher(
             Odometry,
-            "/carla/" + self.role_name + "/ekf_vo",
+            "/vo",
             qos_profile=1)
+
+    def update_imu_data(self, data: Imu):
+        imu_data = Imu()
+
+        imu_data.header.stamp = data.header.stamp
+        imu_data.header.frame_id = "base_footprint"
+
+        imu_data.orientation.x = data.orientation.x
+        imu_data.orientation.y = data.orientation.y
+        imu_data.orientation.z = data.orientation.z
+        imu_data.orientation.w = data.orientation.w
+        imu_data.orientation_covariance = data.orientation_covariance
+
+        imu_data.angular_velocity.x = data.angular_velocity.x
+        imu_data.angular_velocity.y = data.angular_velocity.y
+        imu_data.angular_velocity.z = data.angular_velocity.z
+        imu_data.angular_velocity_covariance = data.angular_velocity_covariance
+
+        imu_data.linear_acceleration.x = data.linear_acceleration.x
+        imu_data.linear_acceleration.y = data.linear_acceleration.y
+        imu_data.linear_acceleration.z = data.linear_acceleration.z
+        data_lac = data.linear_acceleration_covariance
+        imu_data.linear_acceleration_covariance = data_lac
+
+        self.ekf_imu_publisher.publish(imu_data)
+
+    def update_gps_data(self, data: NavSatFix):
+        odom_msg = Odometry()
+
+        odom_msg.header.stamp = data.header.stamp
+        odom_msg.header.frame_id = "base_footprint"
+
+        # Covariance todo: needs tweaking
+        cov_x = 1
+        cov_y = 1
+        cov_z = 1
+
+        lat = data.latitude
+        lon = data.longitude
+        alt = data.altitude
+        x, y, z = self.transformer.gnss_to_xyz(lat, lon, alt)
+
+        odom_msg.pose.pose.position.x = x
+        odom_msg.pose.pose.position.y = y
+        odom_msg.pose.pose.position.z = z
+
+        odom_msg.pose.pose.orientation.x = 1
+        odom_msg.pose.pose.orientation.y = 0
+        odom_msg.pose.pose.orientation.z = 0
+        odom_msg.pose.pose.orientation.w = 0
+
+        odom_msg.pose.covariance = [cov_x, 0, 0, 0, 0, 0,
+                                    0, cov_y, 0, 0, 0, 0,
+                                    0, 0, cov_z, 0, 0, 0,
+                                    0, 0, 0, 999999, 0, 0,
+                                    0, 0, 0, 0, 999999, 0,
+                                    0, 0, 0, 0, 0, 999999]
+
+        self.ekf_vo_publisher.publish(odom_msg)
 
     def run(self):
         """
@@ -60,10 +140,6 @@ class EKFTranslation(CompatibleNode):
         :return:
         """
 
-        def loop(timer_event=None):
-            self.publish_current_pos()
-
-        self.new_timer(self.publish_loop_rate, loop)
         self.spin()
 
 

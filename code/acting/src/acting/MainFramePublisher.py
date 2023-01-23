feat(#108): Trying to fix rotation

code/acting/src/acting/MainFramePublisher.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+from math import cos, sin, radians
+
 import ros_compatibility as roscomp
 import rospy
 from geometry_msgs.msg import PoseStamped
@@ -42,18 +44,49 @@ class MainFramePublisher(CompatibleNode):
             # pos = (-self.current_pos.pose.position.x,
             #        -self.current_pos.pose.position.y,
             #        -self.current_pos.pose.position.z)
-            pos = (self.current_pos.pose.position.x,
-                   self.current_pos.pose.position.y,
-                   40)
-            self.loginfo(str(pos))
+            # pos = (self.current_pos.pose.position.y,
+            #        -self.current_pos.pose.position.x,
+            #        self.current_pos.pose.position.z)
+            # self.loginfo(str(pos))
+
             curr_rot = self.current_pos.pose.orientation
             rot_quat = [curr_rot.x, curr_rot.y, curr_rot.z, curr_rot.w]
-            invert_rot_quat = [1, 0, 0, 0]
-            if not all(v == 0 for v in rot_quat):
-                invert_rot_quat = R.from_quat(rot_quat).inv().as_quat()
-            self.loginfo(str(rot_quat))
+            if all(v == 0 for v in rot_quat):
+                self.loginfo("Invalid rotation data")
+                rot_quat = [1, 0, 0, 0]
+            # rot_quat = R.from_quat(rot_quat).inv().as_quat()
+            # rot_deg = -R.from_quat(rot_quat).
+            # as_euler("xyz", degrees=True)[0] *pi/4 + 90
+            rot_deg = -R.from_quat(rot_quat).as_euler("xyz", degrees=True)[
+                0] + 90
+            # rot_rad = R.from_quat(rot_quat).as_euler("XYZ", degrees=True)
+            # rot_rad =
+            # rot_deg_2 = tf.transformations.euler_from_quaternion(
+            #     [curr_rot.w, curr_rot.x, curr_rot.y, curr_rot.z])
+
+            # self.loginfo(str(radians(rot_deg)))
+            # self.loginfo(str(rot_deg_2))
+
+            pos = [0, 0, 0]
+            pos[0] = cos(radians(rot_deg)) * \
+                self.current_pos.pose.position.x - \
+                sin(radians(rot_deg)) * self.current_pos.pose.position.y
+            pos[1] = sin(radians(rot_deg)) * \
+                self.current_pos.pose.position.x + \
+                cos(radians(rot_deg)) * self.current_pos.pose.position.y
+            pos[2] = self.current_pos.pose.position.z
+            pos[0] = pos[0] * -1
+            pos[1] = pos[1] * -1
+            rot_quat = R.from_euler("xyz", [0, 0, rot_deg], degrees=True) \
+                .as_quat()
+            # if not all(v == 0 for v in rot_quat):
+            #     invert_rot_quat = R.from_quat(rot_quat).inv().as_quat()
+            # self.loginfo(str(pos))
+            # invert_rot_quat = [1, 0, 0, 0]
+            # invert_rot_quat = R.from_euler("xyz", [0, 0, 0], degrees=True)\
+            #     .as_quat()
             br.sendTransform(pos,
-                             invert_rot_quat,
+                             rot_quat,
                              rospy.Time.now(),
                              "global",
                              "hero",
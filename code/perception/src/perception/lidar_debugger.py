7a12e983ea612ff0ed945cc0fd8008b0cad261ff
Tim Dreier <tim.dreier@me.com>
2023-02-06 19:32:42 +0100

    feat(#157): Add ROS package

--- code/perception/src/perception/lidar_debugger.py
+++ code/perception/src/perception/lidar_debugger.py
@@ -1,27 +1,29 @@
 #!/usr/bin/env python
 import rospy
-from std_msgs.msg import String
+from sensor_msgs.msg import Image
 from lidar_utility import LidarUtility
 
-
-def callback(data):
+class LidarDebugger():
     lidar_utility = LidarUtility()
-    lidar_utility.getDepthAtPoint(0, 0)
 
+    def callback(self, data):
+        #rospy.loginfo(data)
+        self.lidar_utility.processLidarImage(data)
 
-def listener():
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # name are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
-    rospy.init_node('lidar_debugger', anonymous=True)
+    def listener(self):
+        # In ROS, nodes are uniquely named. If two nodes with the same
+        # name are launched, the previous one is kicked off. The
+        # anonymous=True flag means that rospy will choose a unique
+        # name for our 'listener' node so that multiple listeners can
+        # run simultaneously.
+        rospy.init_node('lidar_debugger', anonymous=True)
 
-    rospy.Subscriber("/carla/hero/LIDAR", String, callback)
+        rospy.Subscriber("/carla/hero/Center/image", Image, self.callback)
 
-    # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
+        # spin() simply keeps python from exiting until this node is stopped
+        rospy.spin()
 
 
 if __name__ == '__main__':
-    listener()
+    lidar_debugger = LidarDebugger()
+    lidar_debugger.listener()

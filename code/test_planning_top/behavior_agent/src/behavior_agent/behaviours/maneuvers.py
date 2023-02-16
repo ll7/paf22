80d2900253165fa8a239d3faec031a393708414a
JoKircher <josef_kircher@gmx.de>
2023-02-16 12:10:01 +0100

    feat(#176): corrections from merge from main

--- code/test_planning_top/behavior_agent/src/behavior_agent/behaviours/maneuvers.py
+++ code/test_planning_top/behavior_agent/src/behavior_agent/behaviours/maneuvers.py
@@ -92,21 +92,23 @@ class Overtake(py_trees.behaviour.Behaviour):
 
 class Cruise(py_trees.behaviour.Behaviour):
     """
-    This behaviour is the lowest priority one and will be executed when no other behaviour is
-    triggered. It doesn't do much, as in the normal cruising the holding of the lane and speed
-    control is done by different parts of the project. It might be possible to put the activation/
-    deactivation of the ACC here.
+    This behaviour is the lowest priority one and will be executed when no
+    other behaviour is triggered. It doesn't do much, as in the normal cruising
+    the holding of the lane and speed control is done by different parts of the
+    project. It might be possible to put the activation/deactivation of the ACC
+    here.
 
     speed control = acting via speed limits and target_speed
     following the trajectory = acting
     """
     def __init__(self, name):
         """
-        Minimal one-time initialisation. A good rule of thumb is to only include the initialisation
-        relevant for being able to insert this behaviour in a tree for offline rendering to dot
-        graphs.
+        Minimal one-time initialisation. A good rule of thumb is to only
+        include the initialisation relevant for being able to insert this
+        behaviour in a tree for offline rendering to dot graphs.
 
-        Other one-time initialisation requirements should be met via the setup() method.
+        Other one-time initialisation requirements should be met via the
+        setup() method.
 
          :param name: name of the behaviour
         """
@@ -114,10 +116,12 @@ class Cruise(py_trees.behaviour.Behaviour):
 
     def setup(self, timeout):
         """
-        Delayed one-time initialisation that would otherwise interfere with offline rendering of
-        this behaviour in a tree to dot graph or validation of the behaviour's configuration.
+        Delayed one-time initialisation that would otherwise interfere with
+        offline rendering of this behaviour in a tree to dot graph or
+        validation of the behaviour's configuration.
 
-        :param timeout: an initial timeout to see if the tree generation is successful
+        :param timeout: an initial timeout to see if the tree generation is
+        successful
         :return: True, as there is nothing to set up.
         """
         return True
@@ -125,12 +129,14 @@ class Cruise(py_trees.behaviour.Behaviour):
     def initialise(self):
         """
         When is this called?
-        The first time your behaviour is ticked and anytime the status is not RUNNING thereafter.
+        The first time your behaviour is ticked and anytime the status is not
+        RUNNING thereafter.
 
         What to do here?
             Any initialisation you need before putting your behaviour to work.
 
-        This initializes the blackboard to be able to access data written to it by the ROS topics.
+        This initializes the blackboard to be able to access data written to it
+        by the ROS topics.
         """
         self.blackboard = py_trees.blackboard.Blackboard()
 
@@ -144,12 +150,13 @@ class Cruise(py_trees.behaviour.Behaviour):
             - Set a feedback message
             - return a py_trees.common.Status.[RUNNING, SUCCESS, FAILURE]
 
-        This behaviour doesn't do anything else than just keep running unless there is a higher
-        priority behaviour
+        This behaviour doesn't do anything else than just keep running unless
+        there is a higher priority behaviour
 
-        :return: py_trees.common.Status.RUNNING, keeps the decision tree from finishing
+        :return: py_trees.common.Status.RUNNING, keeps the decision tree from
+        finishing
         """
-        rospy.loginfo("Cruising around")
+        # rospy.loginfo("Cruising around")
         return py_trees.common.Status.RUNNING
 
     def terminate(self, new_status):
@@ -157,7 +164,8 @@ class Cruise(py_trees.behaviour.Behaviour):
         When is this called?
         Whenever your behaviour switches to a non-running state.
             - SUCCESS || FAILURE : your behaviour's work cycle has finished
-            - INVALID : a higher priority branch has interrupted, or shutting down
+            - INVALID : a higher priority branch has interrupted, or shutting
+            down
 
         writes a status message to the console when the behaviour terminates
         """

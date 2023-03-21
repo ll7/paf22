769ab784131a1d7f4fdbd75dc3bd2f509ecceb09
MarcoRiedenauer <marco-riedenauer@t-online.de>
2023-03-21 17:40:31 +0100

    feat(#218): Traffic light node is working

--- code/perception/src/TLDNode.py
+++ code/perception/src/TLDNode.py
@@ -7,7 +7,7 @@ from rospy.numpy_msg import numpy_msg
 from ros_compatibility.node import CompatibleNode
 from sensor_msgs.msg import Image
 from std_msgs.msg import String
-
+import cv2
 from traffic_light_detection.src.traffic_light_detection.\
     traffic_light_inference import TrafficLightInference
 from panoptic_segmentation.preparation.labels import name2label
@@ -26,12 +26,11 @@ class TLDNode(CompatibleNode):
 
     def __init__(self, name, **kwargs):
         super().__init__(name, **kwargs)
-
+        self.loginfo("Initializing traffic light detection node...")
         self.effps_sub = None
         self.camera_sub = None
         self.image = None
         self.publisher = None
-        self.loginfo("Initializing traffic light detection node...")
 
         self.role_name = self.get_param("role_name", "hero")
         self.side = self.get_param("side", "Center")
@@ -41,12 +40,13 @@ class TLDNode(CompatibleNode):
         self.setup_subscriptions()
         self.setup_publishers()
         self.traffic_light_id = (name2label["traffic light"]).id
+        self.loginfo("Traffic light detection node initialized.")
 
     def setup_subscriptions(self):
         self.effps_sub = self.new_subscription(
             msg_type=numpy_msg(Image),
             callback=self.handle_segmented_image,
-            topic=f"/carla/{self.role_name}/{self.side}/segmented_image",
+            topic=f"/paf/{self.role_name}/{self.side}/segmented_image",
             qos_profile=1
         )
         self.camera_sub = self.new_subscription(
@@ -59,7 +59,7 @@ class TLDNode(CompatibleNode):
     def setup_publishers(self):
         self.publisher = self.new_publisher(
             msg_type=String,
-            topic=f"/paf/{self.role_name}/{self.side}/segmented_image",
+            topic=f"/paf/{self.role_name}/{self.side}/traffic_light",
             qos_profile=1
         )
 
@@ -69,46 +69,48 @@ class TLDNode(CompatibleNode):
         return model
 
     def predict(self, image: np.ndarray):
-        self.loginfo(f"predicting image shape: {image.shape}")
+        self.loginfo(f"TLDNode predicting image shape: {image.shape}")
         result = self.model(image)
-        result = result.cpu().numpy()
         return result
 
     def handle_image(self, image):
+        self.loginfo(f"TLDNode got image from camera {self.side}")
         image_array = np.frombuffer(image.data, dtype=np.uint8)
         image_array = image_array.reshape((image.height, image.width, -1))
         # remove alpha channel
         image_array = image_array[:, :, :3]
+        image_array = cv2.resize(image_array, (1280, 720),
+                                 interpolation=cv2.INTER_NEAREST)
         self.image = image_array
 
     def handle_segmented_image(self, image):
-        self.loginfo(f"got segmented image from EfficientPS {self.side}")
-        image = rgb2id(image)
-        mask = self.traffic_light_id * 1000 <= image < \
-            (self.traffic_light_id + 1) * 1000
+        self.loginfo(f"TLDNode got segmented image from EfficientPS "
+                     f"{self.side}")
+        image_array = np.frombuffer(image.data, dtype=np.uint8)
+        image_array = image_array.reshape((image.height, image.width, -1))
+        image = rgb2id(image_array)
+        mask = np.ma.masked_outside(image, self.traffic_light_id * 1000,
+                                    (self.traffic_light_id + 1) * 1000)
+        mask = mask.mask
         if not mask.any():
             return
 
         tl_image = np.zeros(image.shape)
         tl_image[mask] = image[mask]
-
         indices = np.nonzero(tl_image[0:tl_image.shape[0] // 2,
                              tl_image.shape[1] // 3:
                              2 * (tl_image.shape[1] // 3)])
-        upper_left = [min(indices[:, 0]), min(indices[: 1])]
-        lower_right = [max(indices[:, 0]), max(indices[: 1])]
+        upper_left = [min(indices[0]), min(indices[1])]
+        lower_right = [max(indices[0]), max(indices[1])]
 
         traffic_light = self.image[upper_left[0]:lower_right[0],
                                    upper_left[1]:lower_right[1]]
         classification = self.predict(traffic_light)
 
         # construct the message
-        msg = Image()
-        msg.header.stamp = roscomp.ros_timestamp(
-            self.get_time(), from_sec=True)
-        msg.header.frame_id = "map"
-        msg.data = str(classification)
-        self.publisher.publish(msg)
+        self.publisher.publish(str(classification))
+        self.loginfo(f"TLDNode classified traffic light "
+                     f"{self.side}")
 
     def run(self):
         self.spin()

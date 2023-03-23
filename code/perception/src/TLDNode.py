774107eca277cf60230ef7fb2e11bebe547cf974
MarcoRiedenauer <marco-riedenauer@t-online.de>
2023-03-23 14:58:12 +0100

    feat(#218): Added segmentation camera and changed to carla 9.14

--- code/perception/src/TLDNode.py
+++ code/perception/src/TLDNode.py
@@ -10,7 +10,7 @@ from std_msgs.msg import String
 import cv2
 from traffic_light_detection.src.traffic_light_detection.\
     traffic_light_inference import TrafficLightInference
-from panoptic_segmentation.preparation.labels import name2label
+from panoptic_segmentation.preparation.labels import name2label, id2label
 from panoptic_segmentation.datasets.panoptic_dataset import rgb2id
 from panopticapi.utils import id2rgb
 
@@ -54,6 +54,12 @@ class TLDNode(CompatibleNode):
         self.camera_sub = self.new_subscription(
             msg_type=numpy_msg(Image),
             callback=self.handle_image,
+            topic=f"/carla/{self.role_name}/Instance_{self.side}/image",
+            qos_profile=1
+        )
+        self.instance_sub = self.new_subscription(
+            msg_type=numpy_msg(Image),
+            callback=self.handle_instance_image,
             topic=f"/carla/{self.role_name}/{self.side}/image",
             qos_profile=1
         )
@@ -90,7 +96,74 @@ class TLDNode(CompatibleNode):
                                  interpolation=cv2.INTER_NEAREST)
         self.image = image_array
 
+    def handle_instance_image(self, image):
+        self.loginfo(f"TLDNode got segmented image from Camera"
+                     f"{self.side}")
+        image_array = np.frombuffer(image.data, dtype=np.uint8)
+        image_array = image_array.reshape((image.height, image.width, -1))
+        image_array = image_array[:, :, 3]
+
+        panoptic = np.zeros(image_array.shape, dtype=np.uint8)
+        formatted = image_array.reshape(-1, image_array.shape[2])
+        segmentIds = np.unique(formatted, axis=0)
+        instance_ids = np.zeros((max(segmentIds[:, 0]) + 1), dtype=np.uint8)
+        for segmentId in segmentIds:
+            semanticId = segmentId[0]
+            labelInfo = id2label[semanticId]
+            if labelInfo.hasInstances:
+                instance_id = 1000 * segmentId[0] \
+                              + instance_ids[segmentId[0]]
+                instance_ids[segmentId[0]] += 1
+            else:
+                instance_id = segmentId[0]
+
+            if labelInfo.ignoreInEval:
+                continue
+            mask = image_array == segmentId
+            mask = mask.all(axis=2)
+            color = [instance_id % 256, instance_id // 256,
+                     instance_id // 256 // 256]
+            panoptic[mask] = color
+        image = rgb2id(panoptic)
+
+        mask = np.ma.masked_inside(image, self.traffic_light_id * 1000,
+                                   (self.traffic_light_id + 1) * 1000 - 1)
+        mask = mask.mask
+
+        tl_image = np.zeros(image.shape)
+        tl_image[mask] = image[mask]
+        msg = Image()
+        msg.header.stamp = roscomp.ros_timestamp(
+            self.get_time(), from_sec=True)
+        msg.header.frame_id = "map"
+        msg.height = 720
+        msg.width = 1280
+        msg.encoding = "rgb8"
+        msg.is_bigendian = 0
+        msg.step = 1280 * 3
+        msg.data = id2rgb(tl_image).tobytes()
+        self.snip_publisher.publish(msg)
+
+        indices = np.nonzero(tl_image[0:tl_image.shape[0] // 2,
+                             tl_image.shape[1] // 3:
+                             2 * (tl_image.shape[1] // 3)])
+
+        if len(indices) == 0:
+            return
+        upper_left = [min(indices[0]), min(indices[1])]
+        lower_right = [max(indices[0]), max(indices[1])]
+
+        traffic_light = self.image[upper_left[0]:lower_right[0],
+                                   upper_left[1]:lower_right[1]]
+        classification = self.predict(traffic_light)
+
+        # construct the message
+        self.class_publisher.publish(str(classification))
+        self.loginfo(f"TLDNode classified traffic light "
+                     f"{self.side}")
+
     def handle_segmented_image(self, image):
+        return
         self.loginfo(f"TLDNode got segmented image from EfficientPS "
                      f"{self.side}")
         image_array = np.frombuffer(image.data, dtype=np.uint8)
@@ -114,12 +187,12 @@ class TLDNode(CompatibleNode):
         msg.data = id2rgb(tl_image).tobytes()
         self.snip_publisher.publish(msg)
 
-        if not mask.any():
-            return
-
         indices = np.nonzero(tl_image[0:tl_image.shape[0] // 2,
                              tl_image.shape[1] // 3:
                              2 * (tl_image.shape[1] // 3)])
+
+        if len(indices) == 0:
+            return
         upper_left = [min(indices[0]), min(indices[1])]
         lower_right = [max(indices[0]), max(indices[1])]
 

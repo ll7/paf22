b83601980c4b313f71ba53329193201c1097b8ee
Julian-Graf <julian.graf@student.uni-augsburg.de>
2023-03-28 23:14:46 +0200

    feat(#162): Prepare for merge

--- code/agent/src/agent/agent.py
+++ code/agent/src/agent/agent.py
@@ -34,7 +34,7 @@ class PAF22Agent(ROS1Agent):
             {'type': 'sensor.other.gnss', 'id': 'GPS',
              'x': 0.7, 'y': -0.4, 'z': 1.60},
             {'type': 'sensor.other.imu', 'id': 'IMU',
-             'x': 0.0, 'y': 0.0, 'z': 0.0, 'roll': 0.0, 'pitch': 0.0,
+             'x': 0.7, 'y': -0.4, 'z': 1.60, 'roll': 0.0, 'pitch': 0.0,
              'yaw': 0.0},
             {'type': 'sensor.opendrive_map', 'id': 'OpenDRIVE',
              'reading_frequency': 1},
